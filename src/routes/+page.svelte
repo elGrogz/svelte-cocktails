0003b8e5bf1e147b26e11af6347339d05833419f
Gregor Gilchrist <gregorgilchrist@gmail.com>
2023-01-25 00:22:43 +0100

    stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
+<script>
+	let cocktailList = '';
+
+	function getCocktails() {
+		console.log('Mixing a cocktail');
+		cocktailList = cocktailList + 'hello';
+	}
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<button on:click={getCocktails}>Get Cocktails</button>
+<pre>{cocktailList}</pre>
