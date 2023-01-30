b4851d602f2ea271ab05317e7585e09742f06bb9
Gregor Gilchrist <gregorgilchrist@gmail.com>
2023-01-30 09:57:52 +0100

    WIP cokctail

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,21 @@
 <script>
+	import { onMount } from 'svelte';
 	let cocktailList = '';
+
+	onMount(() => {
+		console.log('mounted');
+		getCocktails();
+	});
+
+	$: {
+		console.log(
+			`Reactive statement is running because the ${cocktailList} changed. This runs after the other script code has finished and before the HTML code`
+		);
+	}
 
 	function getCocktails() {
 		console.log('Mixing a cocktail');
-		cocktailList = cocktailList + 'hello';
+		// cocktailList = cocktailList + 'hello';
 	}
 </script>
 
@@ -11,3 +23,5 @@
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <button on:click={getCocktails}>Get Cocktails</button>
 <pre>{cocktailList}</pre>
+
+<styles />
