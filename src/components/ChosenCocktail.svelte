a299326febe101c740b8dc676127f34d2b65aed4
Gregor Gilchrist <gregorgilchrist@gmail.com>
2023-03-09 23:50:39 +0100

    animations

--- src/components/ChosenCocktail.svelte
+++ src/components/ChosenCocktail.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
+	import { fly, fade } from 'svelte/transition';
 	import type { Cocktail } from '../types/types';
 
 	export let chosenCocktail: Cocktail;
 </script>
 
-<div class="cocktail-result">
+<div in:fly={{ x: 200, duration: 2000 }} out:fade class="cocktail-result">
 	<h2>{chosenCocktail.strDrink}</h2>
 	<img src={chosenCocktail.strDrinkThumb} alt={chosenCocktail.strDrink} width="200" />
 </div>
