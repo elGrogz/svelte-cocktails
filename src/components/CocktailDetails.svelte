918a95aaf368a0a567348af8cdd3ab4390ceae3d
Gregor Gilchrist <gregorgilchrist@gmail.com>
2023-03-13 08:58:41 +0100

    added ingredients (need to loop through them)

--- src/components/CocktailDetails.svelte
+++ src/components/CocktailDetails.svelte
@@ -4,14 +4,39 @@
 	export let details: Drink;
 </script>
 
-<div class="instructions">
-	<h2>Instructions</h2>
-	<p>{details.strInstructions}</p>
+<div class="details-container">
+	<div class="instructions">
+		<h2>Instructions</h2>
+		<p>{details.strInstructions}</p>
+	</div>
+	<div class="instructions">
+		<h2>Ingredients</h2>
+		<p>{details.strIngredient1}</p>
+		<p>{details.strIngredient2}</p>
+		<p>{details.strIngredient3}</p>
+		<p>{details.strIngredient4}</p>
+		<p>{details.strIngredient5}</p>
+	</div>
 </div>
 
 <style>
+	.details-container {
+		display: flex;
+	}
+
 	.instructions {
-		max-width: 50vw;
+		margin-left: 20px;
+		width: 50%;
 		text-align: center;
+	}
+
+	.instructions {
+		margin-right: 20px;
+		max-width: 50%;
+		text-align: center;
+	}
+
+	p {
+		margin: auto;
 	}
 </style>
